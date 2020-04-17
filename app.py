bf3e3e982810f321435da714620f9a7dd38225e4
Robbe Van Herck <robbe@robbevanherck.be>
2020-04-17 03:02:33 +0200

    Attach a pty to it

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 import mattermost
+import mattermost.ws
 import configparser
+import pty
 from pprint import pprint
 import sys
+import os
+import json
 
 def eprint(message):
    print(message, file=sys.stderr)
+
+def read_from_pty(fd, mm, config):
+    data = os.read(fd, 100000)
+    mm.create_post(config["mattermost"]["channel_id"], f"`{data.decode('utf-8')}`")
+    return data
+
+def handle_ws(mm_ws, data, pty):
+    if data["event"] == "posted":
+        message = json.loads(data["data"]["post"])["message"]
+        if message[0] != '`':
+            os.write(6, bytes(message + "\n", "utf-8"))
+    pass
 
 def main():
     # Read in the config
@@ -32,9 +48,11 @@ def main():
             eprint(f" - {channel['display_name']}: {channel['id']}")
         return
 
-    # Create a message
-    mm.create_post(config["mattermost"]["channel_id"], "TEST")
-    
+    mm_ws = mattermost.ws.MMws(lambda mm_ws, data: handle_ws(mm_ws, data, pty), mm, config["mattermost"]["ws_url"])
+
+    # Spawn a new bash shell, sending all output to Mattermost
+    pty.spawn("/bin/bash", lambda fd: read_from_pty(fd, mm, config))
+    pty.fork()
 
 if __name__ == "__main__":
     main()
